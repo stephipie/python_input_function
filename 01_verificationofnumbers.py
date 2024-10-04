f1e030998f7d53f3890cab3fe1a019b2862c322d
stephipie <stephi.pietschmann@gmail.com>
2024-10-04 21:11:44 +0200

    add task 1

---
+++ 01_verificationofnumbers.py
@@ -0,0 +1,13 @@
+# Define the add_number function
+def check_number():
+    number = int(input("Please enter a number: "))   #We ask user for input
+
+    if number > 0:
+        print("Number is positiv")
+    elif number < 0:
+        print("Number is negativ")
+    else:
+        print("Number is zero.")
+check_number()        
+
+    
