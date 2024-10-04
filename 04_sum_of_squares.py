eec9d14089c1db71d5ecddfa998c01c77550c8ce
stephipie <stephi.pietschmann@gmail.com>
2024-10-04 21:12:33 +0200

    add task 4

---
+++ 04_sum_of_squares.py
@@ -0,0 +1,8 @@
+# Calculate sum of squares of two digits
+# Define a function
+def sum_of_squares():
+    print(a**2 + b**2)
+a = int(input("Please enter one number: \n"))
+b = int(input("Please enter another number. \n"))
+print("The sum of squares is: \n")
+sum_of_squares()
